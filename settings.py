d31d5d103c295562a25fa383a34c9919d9114df4
popcorn <spaziotemporal.com@gmail.com>
2023-02-24 16:45:15 +0100

    linux stuff, settings

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import os
 
 
 class Settings:
-    maximum_number_of_steps = 10000  # call it maximum number of steps
+    maximum_number_of_steps = 100000  # call it maximum number of steps
 
     save_analysis = False
     show_analysis = True
