64017a6c6a0f46a8ea2004240f8411ef70bd5088
popcorn <spaziotemporal.com@gmail.com>
2024-01-11 18:47:32 +0100

    Impremented functions to get test and train error from wrapper booster

--- classes/wrapper_pattern_boosting.py
+++ classes/wrapper_pattern_boosting.py
@@ -6,6 +6,9 @@ from data.load_dataset import split_dataset_by_metal_centers
 from classes.pattern_boosting import PatternBoosting
 from multiprocessing.dummy import Pool as ThreadPool
 import functools
+import numpy as np
+from sklearn import metrics
+from typing import Sequence, Iterable
 
 
 class WrapperPatternBoosting:
@@ -15,7 +18,7 @@ class WrapperPatternBoosting:
 
         if len(pattern_boosting_list) != len(metal_center_list):
             raise ValueError("not enough models for each metal center")
-        self.pattern_boosting_models_list = pattern_boosting_list
+        self.pattern_boosting_models_list: list[PatternBoosting] = pattern_boosting_list
         self.metal_center_list = metal_center_list
 
     def predict(self, graphs_list, boosting_matrix_matrix=None):
@@ -54,6 +57,26 @@ class WrapperPatternBoosting:
         pattern_boosting_model.training(train_dataset, test_dataset)
         return pattern_boosting_model
 
+    def get_wrapper_test_error(self) -> Iterable[float]:
+        ar = np.asarray(self.get_test_models_errors())
+        return np.mean(ar, axis=0)
+
+
+    def get_wrapper_train_error(self) -> Iterable[float]:
+        ar = np.asarray(self.get_train_models_errors())
+        return np.mean(ar, axis=0)
+
+
+    def get_train_models_errors(self) -> Iterable[Sequence[float]]:
+        # it returns a nested list where each row is the vector of errors coming from the model
+        train_model_errors = [model.train_error for model in self.pattern_boosting_models_list]
+        return train_model_errors
+
+    def get_test_models_errors(self) -> Iterable[Sequence[float]]:
+        # it returns a nested list where each row is the vector of errors coming from the model
+        test_model_errors = [model.test_error for model in self.pattern_boosting_models_list]
+        return test_model_errors
+
     def train(self, train_dataset, test_dataset=None):
 
         # some checks for the input format, whether the input dataset it is already divided by metal centers or not
@@ -79,3 +102,19 @@ class WrapperPatternBoosting:
             functools.partial(self.__train_pattern_boosting), input_for_parallelization)
         # -------------------------------------------------------------------------------------------------------------
         return array_of_outputs
+
+    def evaluate(self, dataset: Dataset, boosting_matrix_matrix=None):
+
+        y_pred = self.predict(dataset.get_graphs_list(), boosting_matrix_matrix)
+        labels = dataset.get_labels()
+
+        if Settings.final_evaluation_error == "MSE":
+            model_error = metrics.mean_squared_error(labels, y_pred)
+        elif Settings.final_evaluation_error == "absolute_mean_error":
+            model_error = metrics.mean_absolute_error(labels, y_pred)
+        else:
+            raise ValueError("measure error not found")
+        return model_error
+
+    def get_pattern_boosting_models(self):
+        return self.pattern_boosting_models_list
