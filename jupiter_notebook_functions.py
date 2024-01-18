cc2d132ff0656832cb6a9b7fb44a62c2277c46f7
popcorn <spaziotemporal.com@gmail.com>
2024-01-18 16:36:43 +0100

    making a mess in wrapped pattern boosting analysis jupiter notebook

--- jupiter_notebook_functions.py
+++ jupiter_notebook_functions.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from collections import Counter
 from classes.pattern_boosting import PatternBoosting
+from classes.wrapper_pattern_boosting import WrapperPatternBoosting
 from settings import Settings
 from classes.enumeration.estimation_type import EstimationType
 from data.synthetic_dataset import SyntheticDataset
@@ -45,9 +46,8 @@ import sys
 from collections.abc import Iterable
 
 
-
-def get_XGB_error_and_variable_importance(max_path_length, frequency_matrix, labels, max_number_of_learners,
-                                          xgb_settings=None):
+def get_XGB_error_and_variable_importance_t(max_path_length, pattern_boosting, max_number_of_learners, frequency_matrix,
+                                            xgb_settings=None):
     header = list(frequency_matrix.columns)
 
     columns_to_be_removed = []
@@ -56,6 +56,74 @@ def get_XGB_error_and_variable_importance(max_path_length, frequency_matrix, lab
         if len(path) > max_path_length:
             columns_to_be_removed.append(path)
     print("Frequency matrix ready")
+
+    if isinstance(pattern_boosting, PatternBoosting):
+        x_test = pattern_boosting.create_boosting_matrix_for(pattern_boosting.test_dataset)
+    elif isinstance(pattern_boosting, WrapperPatternBoosting):
+        x_test = pattern_boosting.create_ordered_boosting_matrix(pattern_boosting.test_dataset)
+    else:
+        raise TypeError("Unknown pattern Boosting type")
+
+    x_train = pattern_boosting.boosting_matrix.matrix
+    y_test = pattern_boosting.test_dataset.labels
+    y_train = pattern_boosting.training_dataset.labels
+
+    if xgb_settings is None:
+        xgb_settings = Settings.xgb_model_parameters
+    if isinstance(max_number_of_learners, Iterable):
+        learners_numbers = max_number_of_learners
+    else:
+        learners_numbers = list(range(1, max_number_of_learners, 1))
+    xgb_test_err = []
+    xgb_train_err = []
+    features_importance = None
+    # for i in range(1, max_number_of_learners + 1,10):
+
+    if isinstance(max_number_of_learners, Iterable):
+        learners_numbers = max_number_of_learners
+    for i in learners_numbers:
+        print("Learner number: ", i)
+        # create xgb model
+        # print("number of base learners: ", i)
+        xgb_settings['n_estimators'] = i - 1
+        xgb_model = XGBRegressor(**xgb_settings)
+
+        xgb_model.fit(x_train, y_train)
+
+        # test the model
+        y_test_pred = xgb_model.predict(x_test)
+        y_train_pred = xgb_model.predict(x_train)
+
+        if Settings.final_evaluation_error == "MSE":
+            model_test_error = metrics.mean_squared_error(y_test, y_test_pred)
+            model_train_error = metrics.mean_squared_error(y_train, y_train_pred)
+        elif Settings.final_evaluation_error == "absolute_mean_error":
+            model_test_error = metrics.mean_absolute_error(y_test, y_test_pred)
+            model_train_error = metrics.mean_absolute_error(y_train, y_train_pred)
+        else:
+            raise ValueError("measure error not found")
+        xgb_test_err.append(model_test_error)
+        xgb_train_err.append(model_train_error)
+        features_importance = xgb_model.feature_importances_
+
+    # plot feature importance
+    print("max path length: ", max_path_length)
+    plot_importance(xgb_model)
+    print("max path length: ", max_path_length)
+    plt.show()
+    print("max path length: ", max_path_length)
+    xgb_test_err = np.array(xgb_test_err)
+    return xgb_test_err, xgb_train_err, features_importance
+
+
+def get_XGB_error_and_variable_importance(max_path_length, frequency_matrix, labels, max_number_of_learners,
+                                          xgb_settings=None):
+    header = list(frequency_matrix.columns)
+
+    columns_to_be_removed = []
+    for i, path in enumerate(header):
+        if len(path) > max_path_length:
+            columns_to_be_removed.append(path)
     x_train, x_test, y_train, y_test = train_test_split(frequency_matrix.drop(columns=columns_to_be_removed),
                                                         labels,
                                                         test_size=Settings.test_size,
@@ -67,7 +135,7 @@ def get_XGB_error_and_variable_importance(max_path_length, frequency_matrix, lab
     # for i in range(1, max_number_of_learners + 1,10):
 
     if isinstance(max_number_of_learners, Iterable):
-            learners_numbers=max_number_of_learners
+        learners_numbers = max_number_of_learners
     else:
         learners_numbers = list(range(1, max_number_of_learners, 1))
     xgb_test_err = []
@@ -94,8 +162,8 @@ def get_XGB_error_and_variable_importance(max_path_length, frequency_matrix, lab
             model_train_error = metrics.mean_absolute_error(y_train, y_train_pred)
         else:
             raise ValueError("measure error not found")
-        xgb_test_err.append( model_test_error)
-        xgb_train_err.append( model_train_error)
+        xgb_test_err.append(model_test_error)
+        xgb_train_err.append(model_train_error)
         features_importance = xgb_model.feature_importances_
 
     # plot feature importance
@@ -201,6 +269,77 @@ def plot_graphs(x, y, tittle: str, x_label: str = "", y_label: str = "", show=Tr
     ax.set_xlabel(x_label)
 
     # plt.grid()
+
+    # plot only integers on the x-axis
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    saving_location = data_reader.get_save_location(tittle, '.png')
+
+    if save is True:
+        plt.savefig(saving_location)
+    if show is True:
+        plt.show()
+
+    return fig, ax
+
+
+def plot_graphs_new(x, y, tittle: str, x_label: str = "", y_label: str = "", show=True, save=True, y2=None, x2=None,
+                    max_path_length=[]):
+    plt.style.use('ggplot')
+    if x2 == None:
+        x2 = x
+    fig, ax = plt.subplots()
+    ax.set_ylim(0.0001, 0.0015)
+    # Using set_dashes() to modify dashing of an existing line
+    if len(x) > Settings.tail:
+        ax.plot(x[-Settings.tail:], y[-Settings.tail:], label='')
+    else:
+        ax.plot(x, y, label='Pattern Boosting')
+        for i, max_length in enumerate(max_path_length):
+            ax.plot(x2, y2[i], label='XGB_' + str(max_length))
+
+    ax.legend()
+    ax.set_title(tittle)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+
+    # plt.grid()
+
+    # plot only integers on the x-axis
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    saving_location = data_reader.get_save_location(tittle, '.png')
+
+    if save is True:
+        plt.savefig(saving_location)
+    if show is True:
+        plt.show()
+
+    return fig, ax
+
+
+def plot_graphs_new_temp_funct(x, y, tittle: str, max_number_of_learners, test_err_full_power_xgb, xgb_settings,
+                               test_err_full_power_xgb_on_pattern_boosting_matrix, x_label: str = "", y_label: str = "",
+                               show=True, save=True, y2=None, x2=None, max_path_length=[]):
+    plt.style.use('ggplot')
+    if x2 == None:
+        x2 = x
+    fig, ax = plt.subplots()
+    ax.set_ylim(0.0005, 0.0015)
+    # Using set_dashes() to modify dashing of an existing line
+    if len(x) > Settings.tail:
+        ax.plot(x[-Settings.tail:], y[-Settings.tail:], label='')
+    else:
+        ax.plot(x, y, label='Pattern Boosting')
+        for i, max_length in enumerate(max_path_length):
+            ax.plot(x2, y2[i], label='XGB_' + str(max_length))
+
+    ax.plot(max_number_of_learners, test_err_full_power_xgb, label='XGB depth ' + str(xgb_settings['max_depth']))
+    ax.plot(max_number_of_learners, test_err_full_power_xgb_on_pattern_boosting_matrix, label="XGB on pb matrix")
+
+    # plt.grid()
+    ax.legend()
+    ax.set_title(tittle)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
 
     # plot only integers on the x-axis
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
