fixing error computation in wrapped pattern boosting

classes/wrapper_pattern_boosting.py
@@ -67,13 +67,25 @@ class WrapperPatternBoosting:
         return self.__get_average_of_matrix_of_nested_list_of_errors(self.get_train_models_errors())
 
     @staticmethod
-    def __get_average_of_matrix_of_nested_list_of_errors(errors_lists) -> np.array:
+    def __weighted_average(errors_lists, weights):
+        if len(errors_lists) == len(weights):
+            return [sum(x * y for x, y in zip(sublist, weights)) / sum(weights) for sublist in zip(*errors_lists)]
+        else:
+            return "The length of sublists and weights should be equal"
+
+
+
+    @staticmethod
+    def __get_average_of_matrix_of_nested_list_of_errors(errors_lists, mode='average') -> np.array:
         '''
         :param errors_lists:
+        :param mode: a string 'average', 'max', 'min' to indicate which error measure it should be considered. Average, max or min of the all error made at the i-th step
         :return: an array where each cell correspond to one iteration, since we have multiple base learners each
                     iteration is actually n_base learners iterations, so the final array returned contains the error repeated
                     n_iterations times
         '''
+
+
 
         # filter out the models who are not trained (because their metal center is not contained in the training dataset)
         errors_lists = [error for error in errors_lists if not (error is None or error == [])]
@@ -81,6 +93,8 @@ class WrapperPatternBoosting:
         # FIXME check that the lengths of the error for each model is the same (for now it always is
         #  because we set Setting.train_target_error to zero, but if not, then the train for some
         #  models may stop earlyer)
+
+
 
         errors_lists = np.array(errors_lists)
 
@@ -127,6 +141,9 @@ class WrapperPatternBoosting:
         array_of_outputs = pool.map(
             functools.partial(self.__train_pattern_boosting), input_for_parallelization)
         # -------------------------------------------------------------------------------------------------------------
+        self.test_error=self.get_wrapper_test_error()
+        self.train_error=self.get_wrapper_train_error()
+
         return array_of_outputs
 
     def evaluate(self, dataset: Dataset, boosting_matrix_matrix=None):

main.py
@@ -18,8 +18,6 @@ import sys
 from multiprocessing.dummy import Pool as ThreadPool
 import functools
 
-
-
 if __name__ == '__main__':
     # Testing()
     print("Dataset name: ", Settings.dataset_name)
@@ -29,7 +27,6 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------------------------------------------
     # print("size of dataset: ", asizeof.asizeof(dataset))
     # -----------------------------------------------------------------------------------------------------------
-
 
     """
     splitted_train_datasets_list = split_dataset_by_metal_centers(train_dataset)
@@ -54,28 +51,30 @@ if __name__ == '__main__':
     # ------------------------------------------------------------------------------------------------------------
 
     # part to be parellized
-    #-------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------
     """
     pattern_boosting = PatternBoosting()
     # test_dataset.labels=np.zeros(len(test_dataset.labels))
     pattern_boosting.training(train_dataset, test_dataset)
     final_test_error = pattern_boosting.test_error[-1]
     """
-    #-------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------------------------------
 
     train_dataset, test_dataset = data_reader.split_training_and_test(dataset, Settings.test_size,
                                                                       random_split_seed=Settings.random_split_test_dataset_seed)
 
-    wrapper_pattern_boosting=WrapperPatternBoosting()
-    wrapper_pattern_boosting.train(train_dataset,test_dataset)
+    # wrapper pattern boosting:
+    if Settings.wrapper_boosting is True:
+        wrapper_pattern_boosting = WrapperPatternBoosting()
+        wrapper_pattern_boosting.train(train_dataset, test_dataset)
+        error=wrapper_pattern_boosting.get_wrapper_test_error()
+        final_test_error = wrapper_pattern_boosting.get_wrapper_test_error()[-1]
 
-    final_test_error=wrapper_pattern_boosting.get_wrapper_test_error()[-1]
-
-
-    pattern_boosting = PatternBoosting()
-    pattern_boosting.training(train_dataset, test_dataset)
-    final_test_error = pattern_boosting.test_error[-1]
-
+    else:
+        # pattern boosting
+        pattern_boosting = PatternBoosting()
+        pattern_boosting.training(train_dataset, test_dataset)
+        final_test_error = pattern_boosting.test_error[-1]
 
     print("final test error:\n", final_test_error)
 
@@ -94,10 +93,10 @@ if __name__ == '__main__':
         sys.stdout = original_stdout  # Reset the standard output to its original value
 
 
-    data_reader.save_data(wrapper_pattern_boosting, filename="wrapper_pattern_boosting", directory="results")
-    #data_reader.save_data(pattern_boosting, filename="pattern_boosting", directory="results")
-
-
+    if Settings.wrapper_boosting is True:
+        data_reader.save_data(wrapper_pattern_boosting, filename="wrapper_pattern_boosting", directory="results")
+    else:
+        data_reader.save_data(pattern_boosting, filename="pattern_boosting", directory="results")
 
     """
     analysis = AnalysisPatternBoosting()
@@ -106,4 +105,3 @@ if __name__ == '__main__':
                               save=Settings.save_analysis)
     """
     # analysis.all_analysis(pattern_boosting=pattern_boosting, synthetic_dataset=synthetic_dataset, show=False, save=True)
-