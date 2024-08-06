9802f7a8d3bbb55680fa7c6eb55ea04eace9c55b
popcorn <spaziotemporal.com@gmail.com>
2024-08-06 14:42:44 +0200

    Working on extending boosting matrix

--- adding_features_script.py
+++ adding_features_script.py
@@ -1,11 +1,14 @@
 from classes.extended_boosting_matrix import ExtendedBoostingMatrix
 from data import data_reader
 from classes.dataset import Dataset
+import matplotlib as plt
+import pandas as pd
+import numpy as np
 
 # read the original dataset:
-#dataset_original_graphs = data_reader.read_data_from_directory("data/5k-selection-graphs")
+# dataset_original_graphs = data_reader.read_data_from_directory("data/5k-selection-graphs")
 
-#data_reader.save_dataset_in_binary_file(dataset_original_graphs, filename="5_k_selection_graphs_original")
+# data_reader.save_dataset_in_binary_file(dataset_original_graphs, filename="5_k_selection_graphs_original")
 
 dataset_original_graphs = data_reader.load_dataset_from_binary(filename="5_k_selection_graphs_original")
 
@@ -21,4 +24,63 @@ selected_paths = wrapper_pattern_boosting.get_selected_paths()
 extended_boosting_matrix = ExtendedBoostingMatrix()
 extended_boosting_matrix.create_extend_boosting_matrix(selected_paths=selected_paths,
                                                        dataset=dataset_original_graphs)
-print(extended_boosting_matrix.df)
+extended_boosting_matrix.df.loc[:, : 10000] = np.nan
+print(extended_boosting_matrix.df.dtypes)
+new_df = extended_boosting_matrix.df.astype(pd.SparseDtype(float, fill_value=np.nan))
+print("sparse type")
+print(new_df.dtypes)
+
+from scipy.sparse import csc_matrix
+import matplotlib.pyplot as plt
+
+# First, convert the sparse DataFrame to a scipy sparse matrix
+sparse_matrix = csc_matrix(extended_boosting_matrix.df)
+
+plt.figure(figsize=(10, 10))
+plt.spy(sparse_matrix, markersize=1)
+plt.show()
+
+
+
+
+
+# Get the sparsity pattern for each SparseDtype column
+locations = []
+for column in new_df.columns:
+    sparse_series = new_df[column]
+    if pd.api.types.is_sparse(sparse_series):
+        sparse_array = sparse_series.array
+        # Get the indices of the non-zero entries
+        non_zero_indices = sparse_array.sp_index.to_int_index().indices
+        col_index = new_df.columns.get_loc(column)
+        locations.extend(zip(non_zero_indices, [col_index] * len(non_zero_indices)))
+
+# Unpack the locations to separate lists of rows and columns
+rows, cols = zip(*locations)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(cols, rows, alpha=0.5)
+plt.xlabel('Columns')
+plt.ylabel('Rows')
+plt.gca().invert_yaxis()  # Invert the y-axis to match the matrix representation
+plt.show()
+
+
+
+
+
+
+non_missing_mask = extended_boosting_matrix.df.notna()
+
+# Stack to get indices of non-missing values
+non_missing_indices = non_missing_mask.stack()
+
+# The indices will be a MultiIndex where the first level is the row index and second level is the column index
+rows, cols = zip(*non_missing_indices.index)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(cols, rows, alpha=0.5)
+plt.xlabel('Columns')
+plt.ylabel('Rows')
+plt.gca().invert_yaxis()  # Invert the y-axis to match the matrix representation
+plt.show()
--- classes/extended_boosting_matrix.py
+++ classes/extended_boosting_matrix.py
@@ -10,51 +10,61 @@ from collections import defaultdict
 
 
 class ExtendedBoostingMatrix:
-    def __int__(self):
+
+    def __init__(self):
         self.df: pd.DataFrame | None = None
 
-    def create_extend_boosting_matrix(self, selected_paths: list, dataset: list[nx.classes.multigraph.MultiGraph]):
+
+    def create_extend_boosting_matrix(self, selected_paths: list[tuple],
+                                      dataset: list[nx.classes.multigraph.MultiGraph]):
         # we assume the order of observations in boosting matrix is the same as the order in the variable dataset
         assert isinstance(selected_paths, list)
-        new_columns: pd.DataFrame | None = None
 
         # function to help the retrival of attributes in nx graphs
 
-        self.graphs_list: list[GraphPB] = [GraphPB.from_GraphNX_to_GraphPB(graph) for graph in dataset]
-        for i, graph in enumerate(self.graphs_list):
-            if new_columns is None:
-                all_possible_attributes: list = self.__get_all_possible_attributes(dataset[i])
-                # remove node_position from the list
-                if 'node_position' in all_possible_attributes:
-                    all_possible_attributes.remove('node_position')
-                    all_possible_attributes.remove('node_label')
+        columns_name: list[str] = self.__get_all_possible_node_attributes_in_the_dataset(dataset, selected_paths)
 
-                new_columns = pd.DataFrame(columns=all_possible_attributes)
+        self.graphs_list: list[GraphPB] = [GraphPB.from_GraphNX_to_GraphPB(graph) for graph in dataset]
+
+        if self.df is None:
+            self.df = pd.DataFrame(columns=columns_name)
+        else:
+            # concatenate two datasets
+            self.df = pd.concat([self.df, pd.DataFrame(columns=columns_name)]).drop_duplicates().reset_index(drop=True)
+
+        all_possible_attributes_from_single_graph: set[str] = self.__get_all_possible_attributes(dataset)
+
+        # initialize a list of sets that we will use to create the panda's dataframe
+        list_rows: list[dict] = []
+
+        for i, graph in enumerate(self.graphs_list):
+            # dictionary that contains all the possible values for the same attribute in one graph
+            accumulated_attributes = defaultdict(lambda: [])
 
             for labelled_path in selected_paths:
-                paths = graph.find_labelled_path(labelled_path=labelled_path)
+                numbered_paths_found_in_graph = graph.find_labelled_path(labelled_path=labelled_path)
                 node_attributes: dict = {}
 
-                accumulated_attributes = defaultdict(lambda: [])
 
-                for path in paths:
-                    node_attributes = self.__get_node_attributes_of_nx_graph(graph=dataset[i], node_id=path[-1])
 
-                    for attr in all_possible_attributes:
-                        # Skip if the attribute is not present for this node
-                        if attr not in node_attributes:
-                            continue
+                for numbered_path in numbered_paths_found_in_graph:
+                    node_attributes = self.__get_node_attributes_of_nx_graph(graph=dataset[i],
+                                                                             node_id=numbered_path[-1])
 
-                        # Accumulate the attribute values in a list
-                        accumulated_attributes[attr].append(node_attributes[attr])
+
+                    for attr in node_attributes:
+                        if attr in all_possible_attributes_from_single_graph:
+
+                            # Accumulate the attribute values in a list
+                            accumulated_attributes[self.__get_column_name(labelled_path,attr)].append(node_attributes[attr])
 
                 # --------------------------------
             # Calculate the average of all the accumulated values
             complete_attributes = {attr: np.mean(values) if values else None for attr, values in
                                    accumulated_attributes.items()}
 
-            new_columns = pd.concat([new_columns, pd.DataFrame([complete_attributes])], ignore_index=True)
-        self.df = new_columns
+            list_rows.append(complete_attributes)
+        self.df = pd.DataFrame(list_rows)
 
     @staticmethod
     def find_labels_in_nx_graph(graph: nx.Graph, path: List[int]):
@@ -62,11 +72,17 @@ class ExtendedBoostingMatrix:
             return graph.get_label(path[0])
 
     @staticmethod
-    def __get_all_possible_attributes(graph) -> list:
+    def __get_all_possible_attributes(dataset: list[nx.classes.multigraph.MultiGraph]) -> set[str]:
         unique_attributes = set()
-        for node, attributes in graph.nodes(data=True):
-            unique_attributes.update(attributes.keys())
-        return list(unique_attributes)
+        for graph in dataset:
+            for node, attributes in graph.nodes(data=True):
+                unique_attributes.update(attributes.keys())
+        if 'node_position' in unique_attributes:
+            unique_attributes.remove('node_position')
+        if 'node_label' in unique_attributes:
+            unique_attributes.remove('node_label')
+
+        return unique_attributes
 
     @staticmethod
     # finds all the possible attributes of the node
@@ -75,3 +91,19 @@ class ExtendedBoostingMatrix:
             return graph.nodes[str(node_id)]
         else:
             return None
+
+    @staticmethod
+    def __get_all_possible_node_attributes_in_the_dataset(dataset: list[nx.classes.multigraph.MultiGraph],
+                                                          selected_paths: list[tuple]) -> list[str]:
+        unique_attributes = ExtendedBoostingMatrix.__get_all_possible_attributes(dataset)
+
+        columns_name = set()
+        for path in selected_paths:
+            for attribute in unique_attributes:
+                columns_name.add(ExtendedBoostingMatrix.__get_column_name(path, attribute))
+
+        return sorted(columns_name)
+
+    @staticmethod
+    def __get_column_name(path: tuple, attribute: str):
+        return str(path) + '_' + attribute
