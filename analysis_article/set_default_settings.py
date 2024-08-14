c72ca30a1dcc37a965ded5a7ffff124c3ff0a1b1
popcorn <spaziotemporal.com@gmail.com>
2024-08-14 18:34:16 +0200

    Preparing analysis for paper

---
+++ analysis_article/set_default_settings.py
@@ -0,0 +1,129 @@
+from classes.enumeration.estimation_type import EstimationType
+import platform
+import pandas as pd
+import os
+import multiprocessing as mp
+from settings import Settings
+
+def set_default_settings():
+    Settings.maximum_number_of_steps = 300
+
+    Settings.save_analysis = True
+    Settings.show_analysis = True
+
+    Settings.dataset_name = "5k_synthetic_dataset"  # "5k_synthetic_dataset" "5_k_selection_graphs"  "60k_dataset"
+    Settings.generate_new_dataset = True
+
+    # in the error graph Print only the last N learners
+    Settings.tail = Settings.maximum_number_of_steps + 1
+
+    Settings.wrapper_boosting = False
+
+    # used in wrapped boosting to specify the centers over which split the dataset
+    if Settings.wrapper_boosting is True:
+        Settings.considered_metal_centers = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30,  # first block
+                                    39, 40, 41, 42, 43, 44, 45, 46, 47, 48,  # second block
+                                    57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,  # lanthanides
+                                    72, 73, 74, 75, 76, 77, 78, 79, 80,  # third block
+                                    89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103,  # actinides
+                                    104, 105, 106, 107, 108, 109, 110, 111, 112]
+    else:
+        Settings.considered_metal_centers = None
+
+    # do not expand if the paths are longer than this amount
+    Settings.max_path_length = 101
+
+    # portion of the whole dataset that needs to be used as test dataset
+    Settings.test_size = 0.2
+
+    Settings.target_train_error = 0.0000001
+
+    # it works only if "algorithm" is Xgb_step
+    Settings.update_features_importance_by_comparison = True
+
+    Settings.max_number_of_cores = mp.cpu_count()
+
+    Settings.xgb_model_parameters = {
+        'n_estimators': 1,
+        'booster': 'gbtree',  # gbtree # gblinear
+        'learning_rate': 0.3,
+        "eval_metric": "rmse",
+        "objective": 'reg:squarederror',
+        "reg_lambda": 0,
+        "alpha": 0
+
+    }
+
+    # note in gradient_boosting_model "eval_metric" is assumed to be rmse, be careful when changing it
+
+    if Settings.xgb_model_parameters['booster'] == 'gblinear':
+        Settings.xgb_model_parameters['updater'] = 'coord_descent'  # shotgun
+        Settings.xgb_model_parameters['feature_selector'] = 'greedy'  # cyclic # greedy # thrifty
+        # xgb_model_parameters['top_k'] = 1
+
+    else:
+        Settings.xgb_model_parameters['max_depth'] = 1
+        Settings.xgb_model_parameters['gamma'] = 0
+
+    Settings.plot_tree = False
+
+    Settings.n_of_paths_importance_plotted = 30
+
+    Settings.noise_variance = 0.2
+
+    Settings.random_split_test_dataset_seed = 1
+    Settings.random_coefficients_synthetic_dataset_seed = 1
+    Settings.parallelization = False
+
+    Settings.algorithm = "Xgb_step"  # "Full_xgb" "R" "Xgb_step"
+
+    Settings.graph_label_variable = "target_tzvp_homo_lumo_gap"
+
+    Settings.estimation_type = EstimationType.regression
+    # estimation_type = EstimationType.classification
+
+    # measure used for checkin the final error of the model (to plot error graphs)
+    Settings.final_evaluation_error = "MSE"  # "absolute_mean_error" "MSE"
+
+    # the direcroty is relative to the python file location
+    Settings.r_code_relative_location = 'R_code/m_boost.R'
+
+    # Base Learner used by mboost
+    Settings.r_base_learner_name = "bols"  # "Gaussian", “bbs”, “bols”, “btree”, “bss”, “bns”
+
+    Settings.verbose = False
+
+    # Possible family names for loss function in R mode
+    Settings.family = "Gaussian"
+    # Gaussian: Gaussian
+    # AdaExp: AdaExp
+    # AUC: AUC()
+    # Binomial: Binomial(type=c("adaboost", "glm"), link=c("logit", "probit", "cloglog", "cauchit", "log"), ...)
+    # GaussClass: GaussClass()
+    # GaussReg: GaussReg()
+    # Huber: Huber(d=NULL)
+    # Laplace: Laplace()
+    # Poisson: Poisson()
+    # GammaReg: GammaReg(nuirange=c(0, 100))
+    # CoxPH: CoxPH()
+    # QuantReg: QuantReg(tau=0.5, qoffset=0.5)
+    # ExpectReg: ExpectReg(tau=0.5)
+    # NBinomial: NBinomial(nuirange=c(0, 100))
+    # PropOdds: PropOdds(nuirange=c(-0.5, -1), offrange=c(-5, 5))
+    # Weibull: Weibull(nuirange=c(0, 100))
+    # Loglog: Loglog(nuirange=c(0, 100))
+    # Lognormal: Lognormal(nuirange=c(0, 100))
+    # Gehan: Gehan()
+    # Hurdle: Hurdle(nuirange=c(0, 100))
+    # Multinomial: Multinomial()
+    # Cindex: Cindex(sigma=0.1, ipcw=1)
+    # RCG: RCG(nuirange=c(0, 1), offrange=c(-5, 5))
+
+
+
+    pd.set_option('display.max_columns', None)
+
+    Settings.synthetic_dataset_scenario = 1
+
+    Settings.scenario = 1
+    Settings.set_scenario(1)
---
+++ analysis_article/true_positive_ratio.py
@@ -0,0 +1,191 @@
+import functools
+import warnings
+
+from sklearn import metrics
+import numpy as np
+from classes.boosting_matrix import BoostingMatrix
+from classes.dataset import Dataset
+from settings import Settings
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from collections import Counter
+from classes.pattern_boosting import PatternBoosting
+from settings import Settings
+from classes.enumeration.estimation_type import EstimationType
+from data.synthetic_dataset import SyntheticDataset
+import pandas as pd
+import copy
+from classes.analysis_patternboosting import AnalysisPatternBoosting
+from data.load_dataset import load_dataset
+from data import data_reader
+from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
+from multiprocessing.dummy import Pool as ThreadPool
+from collections import defaultdict
+import copy
+from data import data_reader
+from classes.wrapper_pattern_boosting import WrapperPatternBoosting
+from jupiter_notebook_functions import *
+import warnings
+from typing import List, Tuple, Optional
+from classes.analysis_wrapper_pattern_boosting import AnalysisWrapperPatternBoosting
+import random
+import copy
+from analysis_article.set_default_settings import set_default_settings
+
+
+# %%
+def true_positive_ratio(number_of_simulations=200, synthetic_dataset_scenario=1, noise_variance=0.2,
+                        maximum_number_of_steps=None, save_fig=False):
+    set_default_settings()
+
+
+    Settings.noise_variance = noise_variance
+
+    Settings.scenario = synthetic_dataset_scenario
+    Settings.set_scenario(synthetic_dataset_scenario)
+
+
+    Settings.save_analysis = False
+    Settings.show_analysis = False
+    Settings.dataset_name = "5k_synthetic_dataset"  # "5k_synthetic_dataset" "5_k_selection_graphs"  "60k_dataset"
+    Settings.generate_new_dataset = True
+
+    if synthetic_dataset_scenario == 1 or synthetic_dataset_scenario == 2:
+        Settings.wrapper_boosting = False
+    elif synthetic_dataset_scenario == 3:
+        Settings.wrapper_boosting = True
+
+    if maximum_number_of_steps is None:
+        if synthetic_dataset_scenario == 1 or synthetic_dataset_scenario == 2:
+            Settings.maximum_number_of_steps = 100
+        elif synthetic_dataset_scenario == 3:
+            Settings.maximum_number_of_steps = 300
+
+    final_test_error_vector = []
+    final_train_error_vector = []
+    missed_paths_counter = []
+    n_selected_paths = []
+    n_selected_paths_per_iterations = []
+    overfitting_iteration = []
+
+    true_positive_ratio_1 = []
+    true_positive_ratio_2 = []
+    selected_paths_set = set()
+    cumulative_paths_importance = defaultdict(float)
+    cumulative_times_selected = defaultdict(int)
+    counts = defaultdict(int)
+
+    for i in range(number_of_simulations):
+        print("i")
+        print(i)
+        dataset = load_dataset()
+
+        train_dataset, test_dataset = data_reader.split_training_and_test(dataset, Settings.test_size,
+                                                                          random_split_seed=Settings.random_split_test_dataset_seed)
+
+        # pattern boosting
+        pattern_boosting = PatternBoosting()
+        pattern_boosting.training(train_dataset, test_dataset)
+        final_test_error = pattern_boosting.test_error[-1]
+        final_train_error = pattern_boosting.train_error[-1]
+        final_test_error_vector.append(final_test_error)
+        final_train_error_vector.append(final_train_error)
+        n_selected_paths_per_iterations.append(pattern_boosting.n_selected_paths)
+        true_positive_ratio_1.append(pattern_boosting.true_positive_ratio_1)
+        selected_paths = pattern_boosting.get_selected_paths_in_boosting_matrix()
+        n_selected_paths.append(len(selected_paths))
+        # compute number of times a path is selected and average importance
+        patterns_importance = pattern_boosting.get_boosting_matrix_normalized_columns_importance_values()
+
+        for name, value in zip(pattern_boosting.get_boosting_matrix_header(), patterns_importance):
+            if value > 0.0:
+                cumulative_paths_importance[name] += value
+                counts[name] += 1
+
+        for name, times_selected in zip(pattern_boosting.get_boosting_matrix_header(),
+                                        pattern_boosting.get_number_of_times_path_has_been_selected()):
+            if times_selected > 0:
+                cumulative_times_selected[name] += times_selected
+
+        # compute overfitting iteration
+        synthetic_dataset = SyntheticDataset()
+        overfitting_iteration.append(early_stopping(test_errors=pattern_boosting.test_error, patience=3))
+        missed_paths = []
+        for target_path in synthetic_dataset.target_paths:
+            if target_path not in selected_paths:
+                missed_paths.append(target_path)
+        missed_paths_counter.append(len(missed_paths))
+    averages_importance = {name: cumulative_paths_importance[name] / number_of_simulations for name in
+                           cumulative_paths_importance}
+    averages_times_selected = {name: cumulative_times_selected[name] / number_of_simulations for name in
+                               cumulative_times_selected}
+
+    # Print averages values of results over synthetic dataset
+    print("Averages importances")
+    print_dict_sorted_by_values(averages_importance)
+
+    print("average number of times selected")
+    print_dict_sorted_by_values(averages_times_selected)
+
+    print("final_test_error_vector")
+    print(np.average(final_test_error_vector))
+    print("final_train_error_vector")
+    print(np.average(final_train_error_vector))
+    print("n_selected_paths")
+    print(np.average(n_selected_paths))
+
+    print("n_seleted_paths_vector")
+    print(np.average(n_selected_paths_per_iterations, axis=0))
+
+    print("overfitting_iteration")
+    print(np.average(overfitting_iteration))
+
+    print("coefficients for the syinthetic dataset")
+    synthetic_dataset = SyntheticDataset()
+    print(synthetic_dataset.target_paths)
+    print(synthetic_dataset.coefficients)
+
+    synthetic_dataset = SyntheticDataset()
+    n_target_paths = len(synthetic_dataset.target_paths)
+
+    def plot_tpr_vs_iterations_max_min(true_positive_ratios_per_iteration: list[list[float]], save_fig=True):
+        """
+        Plots the true positive ratio with standard deviation error bars, max, and min
+        against the number of iterations.
+        The iterations are inferred based on the length of the true_positive_ratios list.
+
+        :param true_positive_ratios_per_iteration: A list of list containing the TPR values at each iteration for each simulation.
+        :type true_positive_ratios_per_iteration: list of list of float
+        :returns: None
+        """
+        # Convert to numpy array for easier manipulation
+        np_tpr_per_iteration = np.array(true_positive_ratios_per_iteration)
+
+        # Calculate statistics
+        mean_tpr = np.mean(np_tpr_per_iteration, axis=0)
+        max_tpr = np.max(np_tpr_per_iteration, axis=0)
+        min_tpr = np.min(np_tpr_per_iteration, axis=0)
+        std_tpr = np.std(np_tpr_per_iteration, axis=0)  # Calculate the standard deviation
+
+        iterations = list(range(1, len(mean_tpr) + 1))
+
+        plt.figure(figsize=(10, 6))
+        plt.errorbar(iterations, mean_tpr, yerr=std_tpr, fmt='-o', color='b',
+                     label='True Positive Ratio (TPR) with Std. Dev.', ecolor='red', elinewidth=3, capsize=0)
+        plt.fill_between(iterations, min_tpr, max_tpr, color='b', alpha=0.1, label='Min-Max Range')
+
+        plt.xlabel('Iterations')
+        plt.ylabel('True Positive Ratio')
+        plt.title('True Positive Ratio vs. Iterations with Max, Min, and Std. Dev.')
+        plt.legend()
+        plt.grid(True)
+        if save_fig is True:
+            plt.savefig("true_positive_ratio_with_std_dev.pdf")
+        plt.show()
+
+    plot_tpr_vs_iterations_max_min(true_positive_ratio_1, save_fig=save_fig)
+
+
+true_positive_ratio(number_of_simulations=200, synthetic_dataset_scenario=3, noise_variance=0.2,
+                        maximum_number_of_steps=None, save_fig=True)
