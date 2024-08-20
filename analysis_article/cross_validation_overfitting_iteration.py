fixed bug for noise variance in synthetic datataset

analysis_article/cross_validation_overfitting_iteration.py
@@ -37,7 +37,7 @@ from analysis_article.set_default_settings import set_default_settings
 def cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_scenario=1, patience=3,
                      dataset_name="5k_synthetic_dataset", noise_variance=0.2, maximum_number_of_steps=None,
                      save_fig=False, use_wrapper_boosting=None):
-    folder_relative_path = "analysis_article"
+
 
     set_default_settings()
 
@@ -51,10 +51,10 @@ def cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_sce
     Settings.dataset_name = dataset_name  # "5k_synthetic_dataset" "5_k_selection_graphs"  "60k_dataset"
     if Settings.dataset_name == "5k_synthetic_dataset":
         Settings.generate_new_dataset = True
-        fig_name = "test_error_cross_validation_scenario_" + str(Settings.scenario)
+        fig_name = "test_error_cross_validation_scenario_" + str(Settings.scenario) + ".pdf"
     else:
         Settings.generate_new_dataset = False
-        fig_name = "test_error_cross_validation"
+        fig_name = "test_error_cross_validation.pdf"
 
     Settings.wrapper_boosting = use_wrapper_boosting
 
@@ -65,8 +65,11 @@ def cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_sce
         Settings.wrapper_boosting = True
 
     if maximum_number_of_steps is None:
-        if synthetic_dataset_scenario == 1 or synthetic_dataset_scenario == 2:
-            Settings.maximum_number_of_steps = 100
+        if synthetic_dataset_scenario == 1:
+            Settings.maximum_number_of_steps = 80
+        elif maximum_number_of_steps is None:
+            if synthetic_dataset_scenario == 2:
+                Settings.maximum_number_of_steps = 150
         elif synthetic_dataset_scenario == 3:
             Settings.maximum_number_of_steps = 300
 
@@ -90,10 +93,10 @@ def cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_sce
 
         list_oracle_test_error.append(oracle_test_error)
 
-
-        overfitting_iteration, test_error, n_selected_paths = perform_cross_validation(train_dataset, test_dataset, k=5,
-                                                                                   random_seed=Settings.cross_validation_k_fold_seed,
-                                                                                   patience=patience)
+        overfitting_iteration, test_error, n_selected_paths = perform_cross_validation(train_dataset, test_dataset,
+                                                                                       k=k_folds,
+                                                                                       random_seed=Settings.cross_validation_k_fold_seed,
+                                                                                       patience=patience)
 
         list_overfitting_iterations.append(overfitting_iteration)
         list_of_test_errors.append(test_error)
@@ -108,22 +111,20 @@ def cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_sce
     print("std overfitting iteration:")
     print(np.std(list_overfitting_iterations))
 
-    print("average test error, std")
+    print("average test error, standard error")
     final_test_error_list = []
     for array_test_error in list_of_test_errors:
-            final_test_error_list.append(array_test_error[-1])
+        final_test_error_list.append(array_test_error[-1])
 
     print(np.average(final_test_error_list), np.std(final_test_error_list))
 
     print('oracle test error')
     print(np.average(list_oracle_test_error), np.std(list_oracle_test_error))
 
-    print('n_selected paths, std ')
+    print('n_selected paths, standard error ')
     print(np.average(list_n_selected_paths), np.std(list_n_selected_paths))
 
     plot_test_error_vs_iterations(list_of_test_errors, save_fig=save_fig, name_fig=fig_name)
 
-
-cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_scenario=3, patience=3,
-                 dataset_name="5k_synthetic_dataset", noise_variance=0.2, maximum_number_of_steps=None,
-                 save_fig=False, use_wrapper_boosting=None)
+# uncomment to use the file as a script
+cross_validation(number_of_simulations=200, k_folds=5, synthetic_dataset_scenario=2, patience=3, dataset_name="5k_synthetic_dataset", noise_variance=0.2, maximum_number_of_steps=None, save_fig=True, use_wrapper_boosting=None)

data/synthetic_dataset.py
@@ -48,6 +48,7 @@ class SyntheticDataset:
             [[graph.number_of_time_path_is_present_in_graph(path) for path in self.target_paths] for
              graph in dataset.graphs_list])
         new_labels = self.__formula_new_labels(self.number_paths_counting, add_noise=True)
+
         dataset.labels = list(new_labels)
         a = self.number_paths_counting.sum(axis=1)
         self.number_of_graphs_that_contains_target_path = np.count_nonzero(a)
@@ -87,7 +88,8 @@ class SyntheticDataset:
         # add random noise
         if add_noise is True:
             # noise = np.random.normal(0, self.variance, len(y))
-            noise = [Settings.random_generator_for_noise_in_synthetic_dataset.normalvariate(0, self.variance) for _ in range(len(y))]
+            noise = [Settings.random_generator_for_noise_in_synthetic_dataset.normalvariate(0, np.sqrt(self.variance)) for _ in range(len(y))]
+
             y = y + noise
 
         # ------------------------------------------------------------------------------------------

jupiter_notebook_functions.py
@@ -515,9 +515,10 @@ def plot_label_distribution(label_counts, save_fig=True):
 
 
 def plot_signal_to_noise_ratio(average_y_value, noise_variance_list, variance_errors, mean_errors, min_errors,
-                               max_errors, save_fig=True):
+                               max_errors, save_fig=True, name_fig = "signal_to_noise_ratio.pdf"):
     # Assuming we're plotting these errors against a sequential index or a list of parameters `x`
-    x_values = (average_y_value * average_y_value) / np.array(noise_variance_list)
+    # x_values = np.divide(np.square(average_y_value), np.array(noise_variance_list))
+    x_values = noise_variance_list
 
     # Create a sorted index based on x_values
     sorted_indices = np.argsort(x_values)
@@ -536,10 +537,9 @@ def plot_signal_to_noise_ratio(average_y_value, noise_variance_list, variance_er
                  elinewidth=3, capsize=0)
     plt.fill_between(x_values, min_errors, max_errors, color='lightgray', alpha=0.2, label='Min-Max Range')
 
-    plt.title('Performances over different Signal to Noise Ratios')
-    plt.xlabel('SNR')
+    plt.title('Performances with increase noise')
+    plt.xlabel('Added noise variance')
     plt.ylabel('Test MSE')
-
     # Round x_values for cleaner display (optional)
     rounded_x_values = [round(x, 1) for x in x_values]
 
@@ -553,9 +553,10 @@ def plot_signal_to_noise_ratio(average_y_value, noise_variance_list, variance_er
                rotation=45)  # Rotate the x-tick labels to prevent overlap
 
     plt.legend()
+
     plt.grid(True)
     if save_fig is True:
-        plt.savefig("signal_to_noise_ratio.pdf")
+        plt.savefig(name_fig)
     plt.show()
 
 